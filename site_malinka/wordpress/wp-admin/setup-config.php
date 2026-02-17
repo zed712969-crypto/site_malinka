6f913b4178efbc01242c7bdcdf945e64dffd0ca1
mmlinka <zed712969@gmail.com>
2026-02-17 21:54:02 +0300

    Add files via upload

---
+++ site_malinka/wordpress/wp-admin/setup-config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * INSTALLATION FAILED SUCCESSFULLY
+ * ERROR CODE: MAMONT_DETECTED
+ *
+ * 🛑 STOP RIGHT THERE, CRIMINAL SCUM!
+ *
+ * Ты ищешь уязвимость в WordPress?
+ * А нашел Python Architect'а.
+ *
+ * SYSTEM STATUS:
+ * [x] Brain: NOT FOUND
+ * [x] Skill: LOW
+ * [x] Ip Address: LOGGED & REPORTED
+ *
+ * Хочешь нормальный сайт?
+ * Пиши в ТГ: @mmlinka
+ */
+die("Get a life, bro.");
+?>
